07017c7f30b832e8ad9d761ee42aa554d7618fae
germanmattias <38841129+germanmattias@users.noreply.github.com>
2022-02-09 09:22:11 -0300

    se agrego logica de eliminar datos y editar datos de los productos cargados en la tabla de funkos

--- js/admin.js
+++ js/admin.js
@@ -6,13 +6,20 @@ const modalFunko = new bootstrap.Modal(
 );
 let btnAgregar = document.getElementById("btnAgregar");
 btnAgregar.addEventListener("click", () => {
+  limpiarFormulario();
   modalFunko.show();
 });
 
+
+//existeFunko=true significa que estoy editando un producto
+//exusteFunko=false significa que estoy agregando un nuevo producto
+let existeFunko = false;
+
+
 leerDatos();
 
-window.agregarFunkopop = function (event) {
-  event.preventDefault();
+window.agregarFunkopop = function() {
+
   if (
     validarCodigo(document.getElementById("codigoProducto")) &&
     validarCampoRequerido(document.getElementById("nombreProducto")) &&
@@ -21,7 +28,7 @@ window.agregarFunkopop = function (event) {
     validarCampoRequerido(document.getElementById("descProducto")) &&
     validarCampoRequerido(document.getElementById("imgProducto"))
   ) {
-    console.log("datos correctos");
+
     //crear un nuevo objeto  funko
     let nuevoFunko = new Funko(
       document.getElementById("codigoProducto").value,
@@ -48,7 +55,7 @@ window.agregarFunkopop = function (event) {
     //cerrar la ventana modal
     modalFunko.hide();
   } else {
-    console.log("datos incorrectos");
+
   }
 };
 
@@ -56,6 +63,9 @@ function limpiarFormulario() {
   let formulario = document.getElementById("formProducto");
   formulario.reset();
   //limpiar clase form-control para sacar las tildes del form
+
+  //resetear la variable existeFunko
+    existeFunko = false;
 }
 
 //leer datos del localstorage
@@ -82,12 +92,12 @@ function dibujarDatos(_listaFunkopop) {
     <th scope="row">${_listaFunkopop[i].codigo}</th>
     <td>${_listaFunkopop[i].nombre}</td>
     <td>${_listaFunkopop[i].numSerie}</td>
-    <td>@${_listaFunkopop[i].categoria}</td>
+    <td>${_listaFunkopop[i].categoria}</td>
     <td>${_listaFunkopop[i].descripcion}</td>
     <td>${_listaFunkopop[i].imagen}</td>
     <td>
-      <button class="btn btn-warning">Editar</button>
-      <button class="btn btn-danger" onclick="eliminarFunkopop(this)" id='${_listaFunkopop[i].codigo}'">Eliminar</button>
+      <button class="btn btn-warning" onclick="modificarFunkopop(this)" id='${_listaFunkopop[i].codigo}'>Editar</button>
+      <button class="btn btn-danger" onclick="eliminarFunkopop(this)" id='${_listaFunkopop[i].codigo}'>Eliminar</button>
     </td>
   </tr>
  `;
@@ -97,7 +107,7 @@ function dibujarDatos(_listaFunkopop) {
 }
 
 window.eliminarFunkopop = function(funkopop){
-  console.log('prueba');
+
   Swal.fire({
     title: 'Estas seguro que quieres eliminar el Funkopop?',
     text: 'No podras revertirlo',
@@ -109,6 +119,16 @@ window.eliminarFunkopop = function(funkopop){
     cancelButtonText: 'Cancelar'
   }).then((result) => {
     if(result.isConfirmed){
+    // aqui borrar el producto
+    let funkopopFiltrados = listaFunkopop.filter((producto) =>
+       producto.codigo != funkopop.id
+    )
+    //pasamos los funko filtrados al arreglo principal
+    listaFunkopop = funkopopFiltrados;
+    //guardar en localstorage
+    localStorage.setItem('listaFunkoKey', JSON.stringify(listaFunkopop));
+    //volver a dibujar la tabla con los productos filtrados
+    leerDatos();
 
 
 
@@ -120,4 +140,75 @@ window.eliminarFunkopop = function(funkopop){
       )
     }
   })
+}
+
+window.modificarFunkopop = function(btnEditar){
+  //limpiar los datos de la ventana modal
+  limpiarFormulario();
+  // buscar el objeto a modificar
+  let objetoEncontrado = listaFunkopop.find((producto) =>{
+    return producto.codigo === btnEditar.id;
+  });
+
+
+  // cargar los datos en el formulario
+  document.getElementById('codigoProducto').value = objetoEncontrado.codigo;
+  document.getElementById('nombreProducto').value = objetoEncontrado.nombre;
+  document.getElementById('numSerie').value = objetoEncontrado.numSerie;
+  document.getElementById('categoriaProducto').value = objetoEncontrado.categoria;
+  document.getElementById('descProducto').value = objetoEncontrado.descripcion;
+  document.getElementById('imgProducto').value = objetoEncontrado.imagen;
+  //cambiar el valor de la variable existeFunko
+  existeFunko = true;
+
+  modalFunko.show();
+}
+
+window.guardarFunko = function(event){
+  event.preventDefault();
+  if(existeFunko === true){
+    //en este caso quiero modificar
+    actualizarDatosFunkopop();
+  }else{
+    //en este caso quiero agregar un funko nuevo
+    agregarFunkopop();
+  }
+}
+
+function actualizarDatosFunkopop(){
+  // esta funcion guarda en LS con los datos modificados
+  console.log('modificar');
+
+   let codigo = document.getElementById('codigoProducto').value;
+   let nombre = document.getElementById('nombreProducto').value;
+   let numSerie = document.getElementById('numSerie').value;
+   let categoria = document.getElementById('categoriaProducto').value;
+   let descripcion = document.getElementById('descProducto').value;
+   let imagen = document.getElementById('imgProducto').value;
+
+   //buscar el objeto que quiero modificar y cambiar sus valores
+   for(let i in listaFunkopop){
+     if(listaFunkopop[i].codigo === codigo){
+       //encontre el funko que quiero editar
+       listaFunkopop[i].nombre = nombre;
+       listaFunkopop[i].numSerie = numSerie;
+       listaFunkopop[i].categoria = categoria;
+       listaFunkopop[i].descripcion = descripcion;
+       listaFunkopop[i].imagen = imagen;
+     }
+   }
+// guardar el arreglo de funkopop editados en LS
+localStorage.setItem('listaFunkoKey', JSON.stringify(listaFunkopop));
+//limpiar los datos del formulario
+limpiarFormulario();
+//cerrar ventana modal
+modalFunko.hide();
+//mostrar mensaje de modificacion exitosa
+Swal.fire(
+  'Modificacio Exitosa!',
+  'Se actualizo correctamente su funkupop',
+  'success'
+);
+//leer ls y dibujar los datos actualizados en la tabla
+leerDatos();
 }
